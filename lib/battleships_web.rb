7675a6149c2d30e64b42b86ff86859412dbf7469
Elin <elinnet@gmail.com>
2015-06-24 18:11:01 +0100

    changes to overwrite

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -16,7 +16,6 @@ class BattleshipsWeb < Sinatra::Base
   end
 
   get '/getboard' do
-
     @board = $game.own_board_view $game.player_1
     erb :getboard
   end
@@ -26,21 +25,6 @@ class BattleshipsWeb < Sinatra::Base
     $game.player_1.place_ship Ship.battleship, @coord
     redirect '/getboard'
   end
-  #
-  # get '/getboard' do
-  #   game.player_1.place_ship Ship.battleship, :A5
-  #
-  # end
-
-
-
-
-  # get '/shoot' do
-  #   $coord = params[:cell]
-  #   erb :shoot
-  # end
-
-
 
   run! if app_file == $0
 end
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -29,12 +29,14 @@ feature 'Starting a new game' do
     expect(page).to have_content "x"
   end
 
-  scenario 'It recalls a coordinate' do
-    visit'/placeship'
-    fill_in 'coord', with: 'A5'
-    click_button('Place ship')
-    expect(page).to have_content "A5"
+  # scenario 'It recalls a coordinate' do
+  #   visit'/placeship'
+  #   fill_in 'coord', with: 'A5'
+  #   click_button('Place ship')
+  #   expect(page).to have_content "A5"
+  # end
 
 
-  end
+
+  
 end
