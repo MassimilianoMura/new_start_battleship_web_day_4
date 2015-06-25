aab91e96d452f81259633da097e5967b1dd946ff
Elin <elinnet@gmail.com>
2015-06-25 15:11:15 +0100

    issues with placing ships on the board

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,6 +1,6 @@
-require 'sinatra/base'
+require 'sinatra/base'  
 require 'battleships'
-
+# THIS IS SERVER SIDE
 class BattleshipsWeb < Sinatra::Base
 
   set :views, proc { File.join(root, '..', 'views') }
@@ -22,12 +22,24 @@ class BattleshipsWeb < Sinatra::Base
     erb :getboard_and_place_ships
   end
 
-  # get '/getboard_and_place_ships' do
+  post '/getboard_and_place_ships' do
+    #ship = params[:ships]
     
 
+    coordinates = (params[:x_coord].to_s + params[:y_coord].to_s).to_sym
+    orientation = params[:orientation].to_sym
+
+    $game.player_1.place_ship Ship.submarine, coordinates, orientation
+    @board = $game.own_board_view $game.player_1
+
+    erb :getboard_and_place_ships
+  end
 
 
-    
+
+
+
+
 
   run! if app_file == $0
 end
